77b405ef0c88466139c7741e73f698454a6bb85b
THARUNRAJA <109295291+Tharunraja7@users.noreply.github.com>
2023-12-28 08:50:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from pdf_generator import generate_pdf
 def main():
     input_user_url = input("Enter the URL: ")
     urls = process_url(input_user_url)
-    generate_pdf(urls)
+    pdf_name = input("Enter the name for the PDF: ")  # Ask the user for the PDF name
+    generate_pdf(urls, pdf_name)  # Pass the PDF name to the generate_pdf function
 
 if __name__ == "__main__":
     main()
